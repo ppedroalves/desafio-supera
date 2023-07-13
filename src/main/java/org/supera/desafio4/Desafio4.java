9cb8514e3035938f6facedc3e3d1c75d6f86a1d4
Pedro Antonio <pedroantonioalvesdeoliveira1@gmail.com>
2023-07-13 14:56:04 -0300

    implm desafio 4

--- src/main/java/org/supera/desafio4/Desafio4.java
+++ src/main/java/org/supera/desafio4/Desafio4.java
@@ -1,12 +1,31 @@
 package org.supera.desafio4;
 
-import java.lang.reflect.Array;
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
 public class Desafio4 {
     public static void main(String[] args) {
+        List<String> texts = new ArrayList<>();
 
+        Scanner scanner = new Scanner(System.in);
+        Long textsLength = scanner.nextLong();
+        scanner.nextLine();
+        for (int i = 0; i < textsLength; i++ ){
+            String text = scanner.nextLine();
+            while(text.length() < 2 || text.length() > 100){
+                System.out.println("Texto invalido");
+                text = scanner.next();
+            }
+            texts.add(text);
+        }
 
+        for (String text: texts) {
+            int halfLength = text.length() / 2;
+            String revertFirstHalf = new StringBuilder(text.substring(0, halfLength)).reverse().toString();
+            String revertFinalHalf = new StringBuilder(text.substring(halfLength)).reverse().toString();
+            System.out.println(revertFirstHalf + revertFinalHalf);
+        }
     }
 }
